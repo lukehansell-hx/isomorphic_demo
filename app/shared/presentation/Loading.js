26295c6cba8f99295c57e20a81e2a972b0d80904
Luke Hansell <luke.hansell@holidayextras.com>
2016-09-09 09:01:05 +0100

    outline for loading screen

---
+++ app/shared/presentation/Loading.js
@@ -0,0 +1,10 @@
+const React = require('react')
+// const GridLoader = require('halogen/GridLoader')
+
+require('../style/Loading.less');
+
+module.exports = () => (
+  <div className="load-container">
+    <span className="loading-content">Loading</span>
+  </div>
+)
