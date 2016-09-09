135e03f21c015fca8ab808d829668885e2c0f1d6
Luke Hansell <luke.hansell@holidayextras.com>
2016-09-09 09:01:51 +0100

    adding some style to the App container component

--- app/shared/presentation/App.js
+++ app/shared/presentation/App.js
@@ -5,9 +5,13 @@ require('../style/App.less');
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Super Awesome Holiday Extras App</h1>
-        { this.props.children }
+      <div className="App-container">
+        <header className="App-header">
+          <h1>Super Awesome Holiday Extras App</h1>
+        </header>
+        <div className="App-content">
+          { this.props.children }
+        </div>
       </div>
     )
   }
