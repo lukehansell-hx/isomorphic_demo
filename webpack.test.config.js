e56a1f3cfdad0a30f6fbd92e1801df048fcdc266
Luke Hansell <luke.hansell@holidayextras.com>
2016-09-09 11:23:24 +0100

    adding loaders to test setup

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,6 +1,8 @@
-var nodeExternals = require('webpack-node-externals');
+var nodeExternals = require('webpack-node-externals')
+var defaultServerConfig = require('./webpack.server.config.js')
 
 module.exports = {
   target: 'node', // in order to ignore built-in modules like path, fs, etc.
-  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
-};
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder,
+  module: defaultServerConfig.module
+}
