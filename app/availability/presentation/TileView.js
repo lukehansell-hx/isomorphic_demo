d56f8c5408fbfb890c60b41c0b7f6d283d4b9687
Luke Hansell <luke.hansell@holidayextras.com>
2016-09-09 10:03:41 +0100

    cleaning up prop references

--- app/availability/presentation/TileView.js
+++ app/availability/presentation/TileView.js
@@ -1,11 +1,17 @@
 const React = require('react')
 const { Tile } = require('@holidayextras/ui-toolkit')
 
-module.exports = (props, context) => (
+module.exports = ({
+  products
+}, context) => (
   <div>
-    {props.products.map(product => (
-      <Tile image={product.image} key={product.code}>
-        <h2>{product.name}</h2>
+    {products.map(({
+      image,
+      code,
+      name
+    }) => (
+      <Tile image={image} key={code}>
+        <h2>{name}</h2>
       </Tile>
     ))}
   </div>
